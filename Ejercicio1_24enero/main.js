1754b0d38759540666591f9ca0f1f29b4440d9df
Camila Pinzon <camilapinz18@gmail.com>
2023-01-24 18:13:31 -0500

    Css
    -Simple Css added
    -Verification added (fix)

--- Ejercicio1_24enero/main.js
+++ Ejercicio1_24enero/main.js
@@ -9,23 +9,42 @@ const app = Vue.createApp({
       emptyFields: false,
       userExists: false,
       isSaved: false,
-      isValid: false
+      isValid: false,
+      isError: false,
+      errorMessage: ''
     }
   },
   methods: {
-    validateInput (input, type) {
+    validateInputFormat (input, type) {
       switch (type) {
         case 'text':
-          return /[^a-zA-Z]/.test(input)
+          return !/[^a-zA-Z]/.test(input)
           break
-
-        case 'number':
-          return /^\d+$/.test(input)
-          break
-
         case 'mixed':
+          return /^[a-z][0-9]$/.test(input)
           break
+
+        default:
+          console.log('Invalid input')
       }
+    },
+
+    validateInputs () {
+      let validation = []
+
+      validation.push(this.validateInputFormat(this.name, 'text'))
+      validation.push(this.validateInputFormat(this.surname, 'text'))
+      validation.push(this.validateInputFormat(this.username, 'mixed'))
+
+      validation.map(v => console.log('validaiton', v))
+      this.isValid = validation.find(value => value === false)
+      this.isValid === undefined
+        ? (this.isValid = true)
+        : (this.isValid = false)
+
+      console.log('isvaliod', this.isValid)
+
+      return this.isValid
     },
     createNewUser () {
       console.log(
@@ -52,43 +71,49 @@ const app = Vue.createApp({
         this.emptyFields = false
         //Validate input:
 
-        this.isValid = this.validateInput(this.name, 'text')
-
-        console.log(this.isValid)
-
         //Validate if user already exists:
+        const inputValidation = this.validateInputs()
 
-        usernameExists = this.info.find(
-          user => user.username === this.username.toLowerCase()
-        )
-        if (usernameExists) {
-          this.userExists = true
-          console.log('ya existe', usernameExists)
+        if (inputValidation) {
+          usernameExists = this.info.find(
+            user => user.username === this.username.toLowerCase()
+          )
+          if (usernameExists) {
+            this.userExists = true
+            console.log('ya existe', usernameExists)
 
-          this.username = ''
+            this.username = ''
+          } else {
+            this.userExists = false
+            this.isSaved = true
+            this.info.push({
+              name: this.name,
+              surname: this.surname,
+              fullName: this.name + ' ' + this.surname,
+              username: this.username.toLowerCase(),
+              birthDate: this.birthDate,
+              age: '',
+              password: this.generatePassword()
+            })
+            this.isSaved = true
+            setTimeout(() => {
+              this.isSaved = false
+            }, 1500)
+
+            this.name = ''
+            this.surname = ''
+            this.username = ''
+            this.birthDate = ''
+
+            //this.info.map(m => console.log('Ususarios', m))
+          }
         } else {
-          this.userExists = false
-          this.isSaved = true
-          this.info.push({
-            name: this.name,
-            surname: this.surname,
-            fullName: this.name + ' ' + this.surname,
-            username: this.username.toLowerCase(),
-            birthDate: this.birthDate,
-            age: '',
-            password: this.generatePassword()
-          })
-          this.isSaved = true
+          this.isError = true
+          this.message =
+            'Invalid fields. Please check your name, surname and username'
           setTimeout(() => {
-            this.isSaved = false
+            this.isError = false
           }, 1500)
-
-          this.name = ''
-          this.surname = ''
-          this.username = ''
-          this.birthDate = ''
-
-          this.info.map(m => console.log('Ususarios', m))
         }
 
         //Verificación de entrada de name y surname (Solo texto):
@@ -101,17 +126,14 @@ const app = Vue.createApp({
       const letters = ['u', 'v', 'w', 'x', 'y', 'z']
       const symbols = ['.', '-', '_', '!', '?', '/']
 
-      const nameLength=this.name.length
-      const surnameLength=this.surname.length
-      const maxLength=10
-      const generatedPassword=''
+      const nameLength = this.name.length
+      const surnameLength = this.surname.length
+      const maxLength = 10
+      const generatedPassword = ''
 
-      console.log(nameLength,surnameLength)
+      console.log(nameLength, surnameLength)
 
-      for(let i=0; i<=nameLength;i++){
-
-
-      }
+      for (let i = 0; i <= nameLength; i++) {}
     }
   }
 })
