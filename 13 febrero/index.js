8bf4a0e1c81eba230d8cc75d312f838b2a0e2674
Camila Pinzon <camilapinz18@gmail.com>
2023-02-13 21:20:42 -0500

    logic finished

--- 13 febrero/index.js
+++ 13 febrero/index.js
@@ -2,12 +2,16 @@ const app = Vue.createApp({
   data () {
     return {
       users: [],
+
       countries: [],
       countriesToUse: [],
       username: null,
       password: null,
       isLogin: false,
-      isList: false
+      isList: false,
+      genderSelected: 'all',
+      usersToShow: [],
+      quantity: null
     }
   },
   methods: {
@@ -37,6 +41,7 @@ const app = Vue.createApp({
         }
         console.log('UserToLogin', userToLogin)
       }
+      location.reload()
     },
     loggedUserVerification () {
       if (
@@ -62,32 +67,69 @@ const app = Vue.createApp({
       }
     },
     async fetchData () {
-      await fetch('https://randomuser.me/api/?results=5')
-        .then(response => response.json())
-        .then(data =>
-          data.results.map(user => {
-            this.users.push({
-              photo: user.picture.thumbnail,
-              name: user.name.first,
-              age: user.dob.age,
-              country:
-                `https://countryflagsapi.com/png/${user.location.country}`,
-              email: user.email,
-              phone: user.cell,
-              username: user.login.username,
-              password: user.login.password
+      if (this.users.length <= 15 && this.users.length > 1) {
+        console.log('Ya hay usuarios')
+      } else if (this.users.length < 1) {
+        await fetch('https://randomuser.me/api/?results=15')
+          .then(response => response.json())
+          .then(data =>
+            data.results.map(user => {
+              this.users.push({
+                photo: user.picture.thumbnail,
+                name: `${user.name.first + ' ' + user.name.last}`,
+                age: user.dob.age,
+                country: `https://countryflagsapi.com/png/${user.location.country}`,
+                email: user.email,
+                phone: user.cell,
+                username: user.login.username,
+                password: user.login.password,
+                gender: user.gender
+              })
             })
-          })
-        )
-      //this.getCountryFlag()
+          )
+        //this.getCountryFlag()
+        localStorage.setItem('users', JSON.stringify(this.users))
+
+        console.log('length', this.users.length)
+      }
+    },
+    deleteUser (userToDelete) {
+      console.log('delete', userToDelete.name)
+      //Delete in this.users
+      this.users = this.users.filter(user => {
+        return user.name !== userToDelete.name
+      })
+
+      this.filterByGender()
+
+      localStorage.setItem('users', JSON.stringify(this.users))
+    },
+    filterByGender () {
+      console.log('selected', this.genderSelected)
+      this.quantity = this.users.length
+      console.log("QQQQ",this.quantity)
+      this.usersToShow = this.users.filter(user => {
+        if (this.genderSelected === 'all') {
+          return user
+        } else if (this.genderSelected === 'female') {
+          return user.gender === 'female'
+        } else if (this.genderSelected === 'male') {
+          return user.gender === 'male'
+        }
+      })
+
+      console.log('usersToShow', this.usersToShow)
+      this.usersToShow = this.usersToShow
       localStorage.setItem('users', JSON.stringify(this.users))
     }
   },
+
   created () {
     this.isLogin = true
     this.localStorageVerification()
     this.loggedUserVerification()
     this.fetchData()
+    this.filterByGender()
   }
 })
 
